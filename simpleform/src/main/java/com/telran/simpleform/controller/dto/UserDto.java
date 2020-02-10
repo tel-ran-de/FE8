bc44454eacf689d342e59f8c20e422e7821ce3a9
andron13 <andron13@gmail.com>
2020-02-10 18:26:26 +0100

    work at the school

---
+++ simpleform/src/main/java/com/telran/simpleform/controller/dto/UserDto.java
@@ -0,0 +1,17 @@
+package com.telran.simpleform.controller.dto;
+
+import lombok.Data;
+
+/**
+ * Created for simpleform.
+ * UserDto: andron13
+ * Date: 08.02.2020
+ * Time: 11:34
+ */
+
+@Data
+public class UserDto {
+	private String firstName;
+	private String lastName;
+	private String email;
+}
---
+++ simpleform/src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    console.log("kuku");
+    $('input').hide();
+});
+
