dbd6b1e94c9301a4342fd71195dda612f9e82fab
DenisV <denv03064@gmail.com>
2020-02-11 16:36:52 +0100

    сlass work 2020-02-08

---
+++ simpleform/src/main/java/com/telran/simpleform/controller/SimpleController.java
@@ -0,0 +1,34 @@
+package com.telran.simpleform.controller;
+
+import com.telran.simpleform.controller.dto.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+
+public class SimpleController {
+
+
+    @GetMapping("/form")
+    public String simpleForm(String firstName, String lastName, String email) {
+        log.info("firstName={} lastName={} email={}", firstName,lastName,email);
+        return "OK";
+
+    }
+    @PostMapping("/form")
+    public String simpleFormViaPost(String firstName, String lastName, String email) {
+        log.info("firstName={} lastName={} email={}", firstName,lastName,email);
+        return "OK from post";
+    }
+    @PostMapping("/form-with-object")
+    public String simpleFormViaPost(User user) {
+        log.info("User={}", user);
+        return "OK from post with object";
+    }
+
+}
---
+++ simpleform/src/main/java/com/telran/simpleform/controller/dto/User.java
@@ -0,0 +1,10 @@
+package com.telran.simpleform.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
