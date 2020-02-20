6509a7d821965484046ffa7a8e98c8d94810a766
andron13 <andron13@gmail.com>
2020-02-20 15:40:23 +0100

    hw 18-02-2020

--- simpleform/src/main/resources/static/js/petScript.js
+++ simpleform/src/main/resources/static/js/petScript.js
@@ -1,3 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function () {
     addCrudFunctions('#form1', '#table1');
+
+    createDiv('#form1', '#alert1');
+    createTable('#alert1', '#table1');
 });
+
+function createDiv(formCssSelector, alertCssSelector) {
+// Добавление нового элемента на страницу.
+
+    let $myNewElement = $('<div></div>')
+        .addClass("alert alert-info")
+        .attr('id', alertCssSelector)
+        .attr('role-data', 'alert');
+    $myNewElement.insertAfter(formCssSelector);
+}
+
+function createTable(alertCssSelector, tableSccSelector) {
+    let $myNewElement = $('table')
+        .addClass('table')
+        .attr('id', tableSccSelector);
+
+    function addRowToTable(item) {
+        let row = '<tr>';
+        Object.keys(item).forEach(field => {
+            row += '<td>' + item[field] + '</td>'
+        });
+        row += "/tr";
+        $(tableCssSelector + ' tbody').append(row);
+    };
+
+    $myNewElement.insertAfter(alertCssSelector);
+}
