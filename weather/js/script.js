7c589552c3af631d6c300765fef699889e8f6ea6
andron13 <andron13@gmail.com>
2020-02-21 17:06:21 +0100

    hw weathe 1

--- weather/js/script.js
+++ weather/js/script.js
@@ -1,83 +1,33 @@
-const API_KEY = '07ddf0059c262491e6a6255260e8c44e';
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
+$(document).ready(function () {
 
-const CITY_PICTURES = {
-    "Barcelona": {
-        day: "barcelona.jpg",
-        night: "night_barcelona.jpg"
-    },
-    "Voronezh": {
-        day: "voronezh.jpg",
-        night: "voronezh.jpg"
-    }
-};
+    getWeather();
+    console.log("ok -1");
+});
 
-const WEATHER_FIELDS = [
-    {
-        name: 'City',
-        weatherObject: 'name'
-    },
-    {
-        name: 'Temp.',
-        suffix: '&degC',
-        weatherObject: 'main.temp'
-    },
-    {
-        name: 'Feel',
-        suffix: '&degC',
-        weatherObject: 'main.feels_like'
-    },
-    {
-        name: 'Wind',
-        suffix: ' m/sec',
-        weatherObject: 'wind.speed'
-    },
-    {
-        name: 'Humidity',
-        suffix: '%',
-        weatherObject: 'main.humidity'
-    },
-    {
-        name: 'Pressure',
-        suffix: ' kPa',
-        weatherObject: 'main.pressure'
-    }
-];
+let getWeather = function (APIKEY, city, units) {
 
-$(document).ready(() => {
-    $('form').submit(event => {
+    $("weather-form").submit(function (event) {
         event.preventDefault();
-        console.log(event);
+
+        let url = "http://api.openweathermap.org/data/2.5/weather";
+        const APPIKEY = "daf7d567a6b39bbce964fa94e1a04bfc";
+        let city = "Berlin";
+        let units = "metric";
+        const form = $(this);
 
         $.ajax({
-            method: 'get',
-            url: URL,
-            data: {
-                q: $('select').val(),
-                units: 'metric',
-                appid: API_KEY,
-                },
-            success: (data) => {
-                console.log(data);
-                let table = '';
-                WEATHER_FIELDS.forEach(field => {
-                    table += `<div class = "row">` +
-                        `             <div class="name col-12 col-md-4">` +
-                        `                ${field.name}` +
-                        `             </div>` +
-                        `             <div class="col-12 col-md-8">` +
-                        `                ${eval('data.' + field.weatherObject) + (field.suffix ? field.suffix : '') }                         ` +
-                        `             </div>` +
-                        `          </div>`
-                });
-
-                const cityPicture = CITY_PICTURES[data.name]
-                    ? CITY_PICTURES[data.name]
-                    : '4292857393.jpeg';
-
-                $('body').css("background-image", `url('images/${cityPicture}')`);
-                $('.weather-result').html(table);
+            url: url,
+            method: get,
+            headers: {
+                city: city,
+                units: units,
+                appid: APPIKEY
+            },
+            // data: $('form#myForm').serialize(),
+            success: function (data) {
+                //
             }
         });
-    })
-});
+    });
+    console.log("ok-2");
+};
